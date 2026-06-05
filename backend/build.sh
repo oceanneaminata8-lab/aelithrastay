a04d0066490ad924365f9d1082720c3eb5387213
oceanneaminata8-lab <oceanneaminata8@gmail.com>
2026-06-05 13:12:32 +0100

    chore: add automated build script for database migrations

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Run migrations automatically on deploy
 python manage.py migrate
-python create_admin.py
