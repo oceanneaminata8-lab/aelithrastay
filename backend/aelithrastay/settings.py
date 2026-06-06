bff4776067b6faa0b12803eebe4cf6e99826f4d7
oceanneaminata8-lab <oceanneaminata8@gmail.com>
2026-06-06 13:19:24 +0100

    fix: resolve signup issues and update user creation logic

--- backend/aelithrastay/settings.py
+++ backend/aelithrastay/settings.py
@@ -131,11 +131,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# This tells Django to use WhiteNoise to compress and cache your admin styles
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
