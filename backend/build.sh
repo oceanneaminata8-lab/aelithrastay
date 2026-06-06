720998a84b58dd95fc5e7e44a0b66e75ccd5a819
oceanneaminata8-lab <oceanneaminata8@gmail.com>
2026-06-06 12:40:55 +0100

    chore: move admin creation out of build phase

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Gather all admin and app static assets
+python manage.py collectstatic --no-input
+
 # Run migrations automatically on deploy
 python manage.py migrate
